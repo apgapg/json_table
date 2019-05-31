31faa54e76c45f50c551650034aebd3f448cf5b4
Ayush P Gupta <ayushpguptaapg@gmail.com>
2019-05-31 11:47:31 +0530

    Initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:table_widget/json_table.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final String jsonSample =
+      '[{"name":"Ram","email":"ram@gmail.com","age":23,"income":"10Rs","country":"India","area":"abc","day":"Monday","month":"april"},{"name":"Shyam","email":"shyam23@gmail.com","age":28,"income":"30Rs","country":"India","area":"abc","day":"Monday","month":"april"},{"name":"John","email":"john@gmail.com","age":33,"income":"15Rs","country":"India","area":"abc","day":"Monday","month":"april"},{"name":"Ram","email":"ram@gmail.com","age":23,"income":"10Rs","country":"India","area":"abc","day":"Monday","month":"april"},{"name":"Shyam","email":"shyam23@gmail.com","age":28,"income":"30Rs","country":"India","area":"abc","day":"Monday","month":"april"},{"name":"John","email":"john@gmail.com","age":33,"income":"15Rs","country":"India","area":"abc","day":"Monday","month":"april"},{"name":"Ram","email":"ram@gmail.com","age":23,"income":"10Rs","country":"India","area":"abc","day":"Monday","month":"april"},{"name":"Shyam","email":"shyam23@gmail.com","age":28,"income":"30Rs","country":"India","area":"abc","day":"Monday","month":"april"},{"name":"John","email":"john@gmail.com","age":33,"income":"15Rs","country":"India","area":"abc","day":"Monday","month":"april"},{"name":"Ram","email":"ram@gmail.com","age":23,"income":"10Rs","country":"India","area":"abc","day":"Monday","month":"april"},{"name":"Shyam","email":"shyam23@gmail.com","age":28,"income":"30Rs","country":"India","area":"abc","day":"Monday","month":"april"},{"name":"John","email":"john@gmail.com","age":33,"income":"15Rs","country":"India","area":"abc","day":"Monday","month":"april"}]';
+  bool toggle = false;
+
+  @override
+  Widget build(BuildContext context) {
+    var json = jsonDecode(jsonSample);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Table Widget"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: toggle
+            ? JsonTable(
+                json,
+                tableHeaderBuilder: (String header) {
+                  return Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                    decoration: BoxDecoration(border: Border.all(width: 0.5),color: Colors.grey[300]),
+                    child: Text(
+                      header,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.display1.copyWith(fontWeight: FontWeight.w700, fontSize: 14.0,color: Colors.black87),
+                    ),
+                  );
+                },
+                tableCellBuilder: (value) {
+                  return Container(
+                    padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 2.0),
+                    decoration: BoxDecoration(border: Border.all(width: 0.5, color: Colors.grey.withOpacity(0.5))),
+                    child: Text(
+                      value,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.display1.copyWith(fontSize: 14.0, color: Colors.grey[900]),
+                    ),
+                  );
+                },
+              )
+            : Center(
+                child: Text(jsonSample),
+              ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.grid_on),
+          onPressed: () {
+            setState(
+              () {
+                toggle = !toggle;
+              },
+            );
+          }),
+    );
+  }
+
+  String getPrettyJSONString(jsonObject) {
+    var encoder = new JsonEncoder.withIndent("     ");
+    return encoder.convert(jsonObject);
+  }
+}
---
+++ lib/json_table.dart
@@ -0,0 +1,128 @@
+library table_widget;
+
+import 'package:flutter/material.dart';
+
+typedef TableHeaderBuilder = Widget Function(String header);
+typedef TableCellBuilder = Widget Function(dynamic value);
+
+class JsonTable extends StatefulWidget {
+  final List dataList;
+  final TableHeaderBuilder tableHeaderBuilder;
+  final TableCellBuilder tableCellBuilder;
+
+  JsonTable(this.dataList, {Key key, this.tableHeaderBuilder, this.tableCellBuilder}) : super(key: key);
+
+  @override
+  _JsonTableState createState() => _JsonTableState();
+}
+
+class _JsonTableState extends State<JsonTable> {
+  List<String> headerList = new List();
+  List<List<String>> valueList = new List();
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  void init() {
+    assert(widget.dataList != null && widget.dataList.isNotEmpty);
+    var headerList = extractColumnHeaders(widget.dataList.elementAt(0));
+    assert(headerList != null);
+    this.headerList = headerList;
+  }
+
+  @override
+  void didUpdateWidget(JsonTable oldWidget) {
+    if (oldWidget.dataList != widget.dataList) init();
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Row(
+              children: headerList
+                  .map(
+                    (header) => TableColumn(header, widget.dataList, widget.tableHeaderBuilder, widget.tableCellBuilder),
+                  )
+                  .toList(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<String> extractColumnHeaders(Map<String, dynamic> map) {
+    assert(map != null);
+    var list = new List<String>();
+    var keyList = map.keys;
+    keyList.forEach((key) {
+      list.add(key);
+    });
+    return list;
+  }
+}
+
+class TableColumn extends StatelessWidget {
+  final String header;
+  final List mapList;
+  final TableHeaderBuilder tableHeaderBuilder;
+  final TableCellBuilder tableCellBuilder;
+
+  TableColumn(this.header, this.mapList, this.tableHeaderBuilder, this.tableCellBuilder);
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicWidth(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          tableHeaderBuilder != null
+              ? tableHeaderBuilder(header)
+              : Container(
+                  padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 2.0),
+                  child: Text(
+                    header,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.display1.copyWith(fontWeight: FontWeight.w700, fontSize: 12.0),
+                  ),
+                ),
+
+          Container(
+            child: Column(
+              children: mapList
+                  .map((rowMap) => Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          tableCellBuilder != null
+                              ? tableCellBuilder(getFormattedValue(rowMap[header]))
+                              : Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 2.0),
+                                  child: Text(
+                                    getFormattedValue(rowMap[header]),
+                                    textAlign: TextAlign.center,
+                                    style: Theme.of(context).textTheme.display1.copyWith(fontSize: 12.0, color: Colors.grey[900]),
+                                  ),
+                                ),
+                        ],
+                      ))
+                  .toList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  String getFormattedValue(dynamic value) {
+    return value.toString();
+  }
+}
