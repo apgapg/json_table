63622991560cd4b4334736b4f13ebf589880400c
Ayush P Gupta <ayushpguptaapg@gmail.com>
2019-12-03 09:02:24 +0530

    Fix title

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Json Table Demo',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
